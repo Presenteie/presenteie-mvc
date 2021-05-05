02b719c04216d73e74700397b75d9505ad3a1ba3
FelipePy <felipespsampaiomotasp@gmail.com>
2021-05-05 17:18:13 -0300

    add migrations

--- PresenteieContext.cs
+++ PresenteieContext.cs
@@ -6,7 +6,10 @@ namespace Presenteie
     public class PresenteieContext: DbContext
     {
         public DbSet<User> Users { get; set; }
-
+        
+        public DbSet<List> Lists { get; set; }
+        
+        public DbSet<Item> Items { get; set; }
         public PresenteieContext(DbContextOptions options) : base(options)
         {
         }
