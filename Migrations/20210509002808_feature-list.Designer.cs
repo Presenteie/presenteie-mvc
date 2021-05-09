f521d3e1b6a9326fa8c2691edcb628a3e4d1d1ce
jlucasrods <jlucasrods@gmail.com>
2021-05-08 21:35:36 -0300

    build: new migration for list and items

--- Migrations/20210509002808_feature-list.Designer.cs
+++ Migrations/20210509002808_feature-list.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Presenteie;
@@ -9,9 +10,10 @@ using Presenteie;
 namespace Presenteie.Migrations
 {
     [DbContext(typeof(PresenteieContext))]
-    partial class PresenteieContextModelSnapshot : ModelSnapshot
+    [Migration("20210509002808_feature-list")]
+    partial class featurelist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
