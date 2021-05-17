132019c560ae046151d04f590467fe2499cde5dc
FelipePy <felipespsampaiomotasp@gmail.com>
2021-05-17 18:52:18 -0300

    (Privacy page) Authorize

--- Controllers/ItemsListController.cs
+++ Controllers/ItemsListController.cs
@@ -1,10 +1,12 @@
 using System;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Presenteie.Models;
 
 namespace Presenteie.Controllers
 {
+    [Authorize]
     public class ItemsListController : Controller
     {
         // GET
