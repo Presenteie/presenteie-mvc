f0e6bcba87e197920c5fb669ea7be21ebce45c7e
FelipePy <felipespsampaiomotasp@gmail.com>
2021-05-20 22:22:27 -0300

    pin

---
+++ Controllers/RegisterLisController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presenteie.Controllers
+{
+    public class RegisterLisController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
