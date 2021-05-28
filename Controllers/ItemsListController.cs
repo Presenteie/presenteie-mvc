80cfc9a0e59fba46ba45fd39bad3258bd977006c
FelipePy <felipespsampaiomotasp@gmail.com>
2021-05-28 17:25:37 -0300

    Finalized the functionality of Item

--- Controllers/ItemsListController.cs
+++ Controllers/ItemsListController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
@@ -50,63 +51,6 @@ namespace Presenteie.Controllers
             {
                 controller = "Home",
                 action = "Index"
-            });
-        }
-
-        [HttpGet("ItemsList/Delete/{idItem}")]
-        public IActionResult Delete([FromRoute] long idItem)
-        {
-            var lists = _context.Users.Join(
-                _context.Lists,
-                user => user.Id,
-                list => list.IdUser,
-                (user, list) => list
-                ).ToList();
-           
-            var items = _context.Lists.Join(
-                _context.Items,
-                list => list.Id,
-                item => item.IdList,
-                (list, item) => item
-            ).ToList();
-
-            var item = items.Where(item => item.Id == idItem).FirstOrDefault();
-
-            if (item != null)
-            {
-                var list = lists.Where(list => list.Id == item.IdList).FirstOrDefault();
-                var UserId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
-
-
-                if (list.IdUser == UserId)
-                {
-                    _context.Remove(item);
-                    _context.SaveChanges();
-                }
-
-                return RedirectToRoute(new
-                {
-                    controller = "ItemsList",
-                    action = "Index",
-                    idList = list.Id
-                });
-            }
-
-            return RedirectToRoute(new
-            {
-                controller = "Home",
-                action = "index"
-            });
-        }
-
-        [HttpPost("ItemsList/Create/{idList}")]
-        public IActionResult Create(long id)
-        {
-            return RedirectToRoute(new
-            {
-                controller = "RegisterItem",
-                action = "Index",
-                idList = id
             });
         }
     }
--- Controllers/RegisterItemController.cs
+++ Controllers/RegisterItemController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Linq;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Presenteie.Models;
 
 namespace Presenteie.Controllers
@@ -7,39 +10,44 @@ namespace Presenteie.Controllers
     public class RegisterItemController : Controller
     {
         private readonly PresenteieContext _context;
-        
+
         public RegisterItemController(PresenteieContext presenteieContext)
         {
             _context = presenteieContext;
         }
-        
-        [HttpGet("RegisterItem/{idList}")]
+
+        [HttpGet("Item/Create/{idList}")]
         public IActionResult Index([FromRoute] long idList)
         {
-            ViewBag.IdList = idList;
-            return View();
-        }
-        
-        [HttpPost]
-        public IActionResult Index([FromForm] Item item)
-        {
-            var items = _context.Lists.Join(
-                _context.Items,
-                list => list.Id,
-                item => item.IdList,
-                (list, item) => item
+            Console.WriteLine(idList);
+            var lists = _context.Users.Join(
+                _context.Lists,
+                user => user.Id,
+                list => list.IdUser,
+                (user, list) => list
             ).ToList();
-            var itemVerify = items.Where(item1 => item1.Id == item.Id).FirstOrDefault();
-            
-            if (itemVerify == null) 
+
+            var UserId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
+            var list = lists.Where(list1 => list1.Id == idList && list1.IdUser == UserId).FirstOrDefault();
+
+            if (list != null)
             {
-                _context.Add(item);
+                ViewBag.IdList = list.Id;
+                return View();
             }
-            else
+            var id = idList;
+            return RedirectToRoute(new
             {
-                _context.Items.Update(itemVerify);
-                
-            }
+                controller = "ItemsList",
+                action = "Index",
+                idList = id
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] Item item)
+        {
+            _context.Add(item);
             _context.SaveChanges();
 
             return RedirectToRoute(new
@@ -50,34 +58,102 @@ namespace Presenteie.Controllers
             });
         }
         
-        [HttpPost("RegisterItem/Edit/{idItem}")]
-        public  IActionResult Edit([FromRoute] long idItem)
+        [HttpPost]
+        public IActionResult SaveEdit([FromForm] Item item)
         {
-           var items = _context.Lists.Join(
-                           _context.Items,
-                           list => list.Id,
-                           item => item.IdList,
-                           (list, item) => item
-                       ).ToList();
-                       
-           var item = items.Where(item1 => item1.Id == idItem).FirstOrDefault();
-	   
-	   if(item != null) {
-		   ViewBag.Item = item;
-		   return RedirectToRoute(new
-		   {
-		       controller = "RegisterItem",
-		       action = "Index",
-		       idList = item.IdList
-		   });
-           }
-           return RedirectToRoute( new 
-               {
-           	controller = "Home",
-           	action = "Index"
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return RedirectToRoute(new
+            {
+                controller = "ItemsList",
+                action = "Index",
+                idList = item.IdList
+            });
+        } 
+        
+        [HttpGet("Item/Edit/{idItem}")]
+        public IActionResult Edit([FromRoute] long idItem)
+        {
+            var lists = _context.Users.Join(
+                _context.Lists,
+                user => user.Id,
+                list => list.IdUser,
+                (user, list) => list
+            ).ToList();
+            
+            var items = _context.Lists.Join(
+                _context.Items,
+                list => list.Id,
+                item => item.IdList,
+                (list, item) => item
+            ).ToList();
+            
+            var UserId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
+            var item = items.Where(item1 => item1.Id == idItem).FirstOrDefault();
+            var list = lists.Where(list1 => list1.Id == item.IdList && list1.IdUser == UserId).FirstOrDefault();
+
+            if (list != null)
+            {
+                ViewBag.Item = item;
+                return View("Edit"); 
+            }
+            
+            return RedirectToRoute(new
+            {
+                controller = "ItemsList",
+                action = "Index",
+                idList = item.IdList
             });
         }
         
+        [HttpGet("Item/Delete/{idItem}")]
+        public IActionResult Delete([FromRoute] long idItem)
+        {
+            var lists = _context.Users.Join(
+                _context.Lists,
+                user => user.Id,
+                list => list.IdUser,
+                (user, list) => list
+            ).ToList();
+           
+            var items = _context.Lists.Join(
+                _context.Items,
+                list => list.Id,
+                item => item.IdList,
+                (list, item) => item
+            ).ToList();
+
+            var item = items.Where(item => item.Id == idItem).FirstOrDefault();
+
+            if (item != null)
+            {
+                var list = lists.Where(list => list.Id == item.IdList).FirstOrDefault();
+                var UserId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
+
+
+                if (list.IdUser == UserId)
+                {
+                    _context.Remove(item);
+                    _context.SaveChanges();
+                }
+
+                return RedirectToRoute(new
+                {
+                    controller = "ItemsList",
+                    action = "Index",
+                    idList = list.Id
+                });
+            }
+
+            return RedirectToRoute(new
+            {
+                controller = "Home",
+                action = "index"
+            });
+        }
     }
     
+    
+
 }
