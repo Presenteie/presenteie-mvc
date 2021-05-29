79a82c8b6adf1a864a7c0893ffd9398cc6b819f5
Pedro <pholiveira2002@outlook.com>
2021-05-29 01:30:49 -0300

    Atualização cadastrar lista 2

--- Controllers/RegisterListController.cs
+++ Controllers/RegisterListController.cs
@@ -1,35 +1,82 @@
-using Microsoft.AspNetCore.Mvc;
-using Presenteie.Models;
 using System;
-using System.Security.Claims;
 using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Presenteie.Models;
 
 namespace Presenteie.Controllers
 {
     public class RegisterListController : Controller
     {
         private readonly PresenteieContext _context;
-
+        [BindProperty]
+        public List list {get; set;}
         public RegisterListController(PresenteieContext presenteieContext)
         {
             _context = presenteieContext;
         }
 
-        [HttpGet]
+        [HttpGet ("List/Create")]
         public IActionResult Index()
         {
+            var userId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
+            ViewBag.UserId = userId;
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index([FromForm] List list)
+        public IActionResult Create([FromForm] List list)
         {
-            var userId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
-            //_context.Lists.Add(list);
-            //return View();
-            return RedirectToAction("Index", "Home");   
+            Console.WriteLine(list.EventDate);
+            list.CreatedDate = DateTime.Now;
+            _context.Add(list);
+            _context.SaveChanges();
+            return RedirectToRoute(new{
+                controller = "Home",
+                action = "Index"
+            });   
         }
 
+        [HttpGet("List/Edit/{idList}")]
+        public IActionResult Index([FromRoute] long idList)
+        {
+            var lists = _context.Users.Join(
+                _context.Lists,
+                user => user.Id,
+                list => list.IdUser,
+                (user, list) => list
+            ).ToList();
+            var userId = long.Parse(User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value);
+            list = lists.Where(list => list.Id == idList && list.IdUser == userId).FirstOrDefault();
+            if (list != null) {
+                
+                Console.WriteLine(list.Description);
+                ViewBag.List = list;
+                return View("Edit");
+            }
+
+            
+            return RedirectToRoute(new {
+
+                controller = "Home",
+                action = "Index"
+            });
+        }
+
+        [HttpPost("List/Edit")]
+        public IActionResult Edit([FromForm] List list)
+    
+        {
+
+            _context.Entry(list).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToRoute(new {
+
+                controller = "Home",
+                action = "Index"
+            });
+        }
 
     }
 }
